c1a225dbe891ba0f9f55f2257a832d2e22a18b26
mykhailobiesiedin <122889486+mykhailobiesiedin@users.noreply.github.com>
2023-05-11 16:11:09 +0300

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -4,21 +4,4 @@ public class Dog extends Animal {
     public Dog(String name) {
         super(name, 500, 10);
     }
-//    @Override
-//    public int Run(int runLimit) {
-//        if (runLimit > 500) {
-//            return 0;
-//        } else {
-//            return runLimit;
-//        }
-//    }
-//
-//    @Override
-//    public int Swim(int swimLimit) {
-//        if (swimLimit > 10) {
-//            return 0;
-//        } else {
-//            return swimLimit;
-//        }
-//    }
 }
