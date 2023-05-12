21e5c0e20a0138c59f7eb889c023d1583c882dd6
mykhailobiesiedin <122889486+mykhailobiesiedin@users.noreply.github.com>
2023-05-12 20:22:47 +0300

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -4,4 +4,23 @@ public class Cat extends Animal {
     public Cat(String name) {
         super(name, 200, 0);
     }
+
+    @Override
+    public void  run(int runLimit) {
+        if (runLimit > 200) {
+            System.out.println("Cat can not run more than " + 200 + " meters");
+        } else {
+            System.out.println("Cat run : " + runLimit + " meters");
+        }
+    }
+
+    @Override
+    public void swim(int swimLimit) {
+
+        if (swimLimit > 0) {
+            System.out.println("Cat can not swim ");
+        } else {
+            System.out.println("Cat can not swim");;
+        }
+    }
 }
