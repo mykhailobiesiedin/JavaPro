0416072bc126e33a48803427764cebe12de8bc4c
mykhailobiesiedin <122889486+mykhailobiesiedin@users.noreply.github.com>
2023-05-12 20:21:59 +0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,11 +1,9 @@
 package homework_2;
 
-public class Animal {
+public abstract class Animal {
     protected int runLimit;
     protected int swimLimit;
     protected String name;
-
-
 
     public Animal(String name, int runLimit, int swimLimit) {
         this.runLimit = runLimit;
@@ -13,20 +11,8 @@ public class Animal {
         this.name = name;
     }
 
-public void Run(int distance) {
-    if (distance <= runLimit) {
-        System.out.println( name + " run " + distance + " meters.");
-    } else {
-        System.out.println(name + " can not run " + distance + " meters.");
-    }
-}
+ public abstract void run(int distance);
 
-    public void Swim(int distance) {
-        if (distance <= swimLimit) {
-            System.out.println(name + " swam " + distance + " meters.");
-        } else {
-            System.out.println(name + " can not swim " + distance + " meters.");
-        }
-    }
+ public abstract void swim(int distance);
 }
 
